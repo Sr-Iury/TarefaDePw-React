bee3dc7ad6e5786a7be18ee574cce5a648c93e8b
Sr-Iury <iury.silvafim@gmail.com>
2024-12-10 17:40:05 -0300

    Tarefa concluída com êxito

---
+++ IuryFimDaSilva/iuryfimdasilva/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          NOME: IURY FIM DA SILVA <br></br>
+          TURMA: 3°A Informática <br></br>
+          CURSO: Informática <br></br>
+          MATRICULA 2022102020024 <br></br>
+          CPF: 048.824.772-11 <br></br>
+          RG: 14975-14 <br></br>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
